a045e35ec721d12cfd74e5b5bc35e216bcc3e532
Sergii Kostenko <makeitdoubleplz@gmail.com>
2017-02-06 18:01:07 +0200

    done

--- src/app/name-filter.ts
+++ src/app/name-filter.ts
@@ -4,14 +4,10 @@ import {Pipe} from '@angular/core';
   name: 'namefilter'
 })
 export class NameFilter {
-    transform(value: any, args: string[]): any {
+    transform(value: any, args: string): any {
         if (args){
-            var argsArr = [];
-            for (var i = 0; i < args.length; i++) {
-                argsArr.push(args[i]);
-            }
-            var filter = argsArr.join('');
-            return value.filter(user => user.name.toLocaleLowerCase().indexOf(filter) > -1);
+            console.log(args);
+            return value.filter(user => user.name.toLocaleLowerCase().indexOf(args.toLocaleLowerCase()) > -1);
         }
         return value;
     }
